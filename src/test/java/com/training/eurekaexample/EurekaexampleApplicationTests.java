411f9e2ec2218157c0f22b2c35e3130f3eb956ef
Gowrisankar1421 <gowrisankar1129@gmail.com>
2020-05-21 23:29:37 +0530

    initializarion of eureka

---
+++ src/test/java/com/training/eurekaexample/EurekaexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.eurekaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
